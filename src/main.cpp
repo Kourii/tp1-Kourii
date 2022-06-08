3bd4d2beb6530d94a6de72b956d38139f85c51c9
Kourii <79379864+Kourii@users.noreply.github.com>
2022-06-08 14:26:33 -0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,43 @@
 #include <Arduino.h>
 #include <util/delay.h>
 
-#define boton1 ((PIND >> 4) & 0x01)
-#define boton2 ((PIND >> 5) & 0x01)
-#define boton3 ((PIND >> 6) & 0x01)
-#define boton4 ((PIND >> 7) & 0x01)
+#define bot1 ((PIND >> 4) & 0x01)
+#define bot2 ((PIND >> 5) & 0x01)
+#define bot3 ((PIND >> 6) & 0x01)
+#define bot4 ((PIND >> 7) & 0x01) 
 
 #define SET_PIN(PORT, PIN) (PORT |= 1 << PIN)
 #define CLEAR_PIN(PORT, PIN) (PORT &= ~(1 << PIN))
 
-int main(void)
-{
+int main(void){
   DDRD &= 0x0f;
   DDRB |= 0x0f;
-  while (1)
-  {
-    if (boton1 != 1)
-    {
-      SET_PIN(PORTB, 0);
+  while (1){
+    
+    if (bot1 != 1){
+      SET_PIN(PORTB,0);
     }
-
-    if (boton2 != 1)
-    {
+    else{
+      CLEAR_PIN(PORTB,0);
+    }
+    if (bot2 != 1){
       SET_PIN(PORTB, 1);
     }
-
-    if (boton3 != 1)
-    {
+    else{
+      CLEAR_PIN(PORTB,1);
+    }
+    if (bot3 != 1){
       SET_PIN(PORTB, 2);
     }
-
-    if (boton4 != 1)
-    {
+    else{
+      CLEAR_PIN(PORTB,2);
+    }
+    if (bot4 != 1){
       SET_PIN(PORTB, 3);
+  
+    }
+    else{
+      CLEAR_PIN(PORTB,3);
     }
   }
 
